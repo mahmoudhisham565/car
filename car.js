855ca0703e1fa079d3ad359d41196a621bf11134
mahmoud hisham <153205181+mahmoudhisham565@users.noreply.github.com>
2023-12-13 04:30:53 +0200

    Add files via upload

---
+++ car.js
@@ -0,0 +1,26 @@
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
+let btn4 = document.getElementById('btn4')
+let btn5 = document.getElementById('btn5')
+let btn6 = document.getElementById('btn6')
+let maincar2 = document.getElementById('maincar2')
+
+btn1.onclick = function() {
+    maincar2.src = "audia1.jpg"
+}
+btn2.onclick = function() {
+    maincar2.src = "golf6.jpg"
+}
+btn3.onclick = function() {
+    maincar2.src = "toyotacamry.jpg"
+}
+btn4.onclick = function() {
+    maincar2.src = "bmw320.jpg"
+}
+btn5.onclick = function() {
+    maincar2.src = "benz.jpg"
+}
+btn6.onclick = function() {
+    maincar2.src = "passatcc.jpg"
+}
